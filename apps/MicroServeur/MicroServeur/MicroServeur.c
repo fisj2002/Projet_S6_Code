ba97b55b2752d543f4605974f36f91d8bdb72e09
Édouard Denommée <dene2303@usherbrooke.ca>
2018-09-27 13:19:59 -0400

    Cree le projet AtmelStudio cote operateur

---
+++ apps/MicroServeur/MicroServeur/MicroServeur.c
@@ -0,0 +1,18 @@
+/*
+ * MicroServeur.c
+ *
+ * Created: 2018-09-27 12:18:41
+ *  Author: dene2303
+ */ 
+
+
+#include <avr/io.h>
+#include "Uart.h"
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
