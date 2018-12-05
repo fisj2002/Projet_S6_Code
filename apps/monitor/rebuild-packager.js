9e1fc6e7e6ad3b96df4c24aa3a1b8e01891ac587
MoronixProduct3 <moronicproduct3@gmail.com>
2018-12-04 22:08:48 -0500

    PT-62 Fixed build procedure

--- apps/monitor/rebuild-packager.js
+++ apps/monitor/rebuild-packager.js
@@ -34,57 +34,75 @@ const common = require('./node_modules/electron-packager/common')
 const fs = require('fs')
 const packager = require('electron-packager')
 const usage = fs.readFileSync('./node_modules/electron-packager/usage.txt').toString()
+const copyfiles = require('copyfiles')
 
 var args = common.parseCLIArgs(process.argv.slice(2))
 
 // temporary fix for https://github.com/nodejs/node/issues/6456
 var stdioWriters = [process.stdout, process.stderr]
 stdioWriters.forEach(function (stdioWriter) {
-  if (stdioWriter._handle && stdioWriter._handle.setBlocking) {
-    stdioWriter._handle.setBlocking(true)
-  }
+    if (stdioWriter._handle && stdioWriter._handle.setBlocking) {
+        stdioWriter._handle.setBlocking(true)
+    }
 })
 
-function printUsageAndExit (isError) {
-  var print = isError ? console.error : console.log
-  print(usage)
-  process.exit(isError ? 1 : 0)
+function printUsageAndExit(isError) {
+    var print = isError ? console.error : console.log
+    print(usage)
+    process.exit(isError ? 1 : 0)
 }
 
 if (args.help) {
-  printUsageAndExit(false)
+    printUsageAndExit(false)
 } else if (args.version) {
-  if (typeof args.version !== 'boolean') {
-    console.error('--version does not take an argument. Perhaps you meant --app-version or --electron-version?\n')
-  }
-  console.log(common.hostInfo())
-  process.exit(0)
+    if (typeof args.version !== 'boolean') {
+        console.error('--version does not take an argument. Perhaps you meant --app-version or --electron-version?\n')
+    }
+    console.log(common.hostInfo())
+    process.exit(0)
 } else if (!args.dir) {
-  printUsageAndExit(true)
+    printUsageAndExit(true)
 }
 
 // Native module rebuild function
-function afterCopyRebuild (buildPath, electronVersion, platform, arch, callback)
-{
+function nativePackageRebuild(buildPath, electronVersion, platform, arch, callback) {
     console.log('Rebuilding native modules');
     electronRebuild.rebuild({ buildPath, electronVersion, arch })
-      .then(() => callback())
-      .catch((error) => callback(error));
+        .then(() => callback())
+        .catch((error) => callback(error));
 }
 
-// Adding module rebuild options from native modules
+// Required files must be copied
+function includeFiles(appDirectory) {
+    console.log('Copying required files');
+
+    copyfiles(['./gmaps-api.txt','./bee/*.json','./pages/*', './' + appDirectory], {}, (err) => {
+        if (err) {
+            console.error('Failed to copy files:')
+            console.error(err)
+        }
+    })
+}
+
+// Adding after copy functions
 if (args.afterCopy)
-    args.afterCopy.push(afterCopyRebuild);
+    args.afterCopy.push(nativePackageRebuild);
 else
-    args.afterCopy = [afterCopyRebuild];
+    args.afterCopy = [nativePackageRebuild];
 
 packager(args)
-  .then(function done (appPaths) {
-    if (appPaths.length > 1) console.error('Wrote new apps to:\n' + appPaths.join('\n'))
-    else if (appPaths.length === 1) console.error('Wrote new app to', appPaths[0])
-    return true
-  }).catch(function error (err) {
-    if (err.message) console.error(err.message)
-    else console.error(err, err.stack)
-    process.exit(1)
-  })
+    .then(function done(appPaths) {
+        if (appPaths.length > 1) console.error('Wrote new apps to:\n' + appPaths.join('\n'))
+        else if (appPaths.length === 1) console.error('Wrote new app to', appPaths[0])
+
+        // Copy files to apps
+        appPaths.forEach((path) => {
+            includeFiles(path);
+        })
+
+        return true
+    }).catch(function error(err) {
+        if (err.message) console.error(err.message)
+        else console.error(err, err.stack)
+        process.exit(1)
+    })
